2c1e8ba893558834dfaabb2f4baf6411d7cdffcc
Gaurav-programing <gaurav.twist@gmail.com>
2021-03-21 22:12:25 +0530

    MY pro

---
+++ MeldMerge/TableOFNum1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TableOFNum1 {
+
+	public static void main(String[] args)
+	{
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter the number ");
+	        int t = sc.nextInt();
+	        
+	        for (int i = 1; i <=10; i++) 
+	        {
+			      System.out.println(t+"*"+i +"=" + t*i);	
+			}
+
+	}
+
+}
